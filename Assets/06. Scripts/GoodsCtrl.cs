8da3fa8d7c6d03a018c6bc0b3d4acae47b0a382f
kimhyelim <limma0608@naver.com>
2016-03-25 05:42:55 +0900

    수레 충돌 부분 주석 추가

--- Assets/06. Scripts/GoodsCtrl.cs
+++ Assets/06. Scripts/GoodsCtrl.cs
@@ -3,43 +3,26 @@ using System.Collections;
 
 public class GoodsCtrl : MonoBehaviour
 {
-
-    private float gravity;//중력가속도
-    private int startAngle;//시작 각도
     public Rigidbody body;
-    private float speed;//
+    private float speed;
+
     private float v_x;//x축으로 이동
     private float v_y;//y축으로 이동
     private float v_z;//y축으로 이동
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
     public void fall(Vector3 pos)
     {
 
-        body.detectCollisions = false;
-        speed = Random.Range(10.0f, 20f);//스피드 랜덤 
+        body.detectCollisions = false;          //물체 끼리 충돌 off
+        speed = Random.Range(10.0f, 20f);       //스피드 값 랜덤 
 
 
+        //물품이 떨어지는 방향
 		v_x = pos.x + Random.Range( -pos.x, pos.x );
-		v_y = ( pos.y + 0.1f ) * 30.0f;
+		v_y = ( pos.sqrMagnitude+ 0.1f ) * 10.0f;
 		v_z = pos.z + Random.Range( -pos.z, pos.z );
 
 
-		//v_x = pos.x + Random.Range( -pos.x * 0.2f, pos.x * 0.2f );
-		//v_y = pos.magnitude * 0.3f;
-		//v_z = pos.z + Random.Range( -pos.z * 0.2f, pos.z * 0.2f );
-
-		//Vector3 moveDir = (Vector3.forward * v_z*2) + (Vector3.right * v_x*2) + (Vector3.up * 100);
-		//body.AddForce(moveDir * speed);
 		body.AddForce(new Vector3(v_x, v_y, v_z) * speed);
         StartCoroutine(dropTime());
 
@@ -48,17 +31,17 @@ public class GoodsCtrl : MonoBehaviour
     {
 
 		yield return new WaitForSeconds( 0.3f );
-		body.detectCollisions = true;
+		body.detectCollisions = true;           //충돌 on
 		yield return new WaitForSeconds(2.0f);
         Destroy(this.gameObject);
 
     }
     void OnCollisionEnter(Collision coll)
     {
+        //떨어진 물품에 충돌할 경우 다시 추가
         if(coll.gameObject.tag==("CART"))
         {
             Destroy(this.gameObject);
-			//coll.gameObject.GetComponent<WagonCtrl>().goodsCount += 1;
 			Item item = GameMng.Inst.getItem( 0 );
 			if ( item != null ) {
 				item.count += 1;
--- Assets/06. Scripts/WagonCtrl.cs
+++ Assets/06. Scripts/WagonCtrl.cs
@@ -3,17 +3,16 @@ using System.Collections;
 
 public class WagonCtrl : MonoBehaviour
 {
+    public int[] range = new int[5];                //떨어트리는 물품 사이 값
 
-    public int[] range = new int[5];
-    public GameObject good;
+    public GameObject preObj;
 
-    public GameObject[] goods;
+    private GameObject[] goods;                     //떨어트리는 물품들
 
-    private bool isCrash;
+    private bool isCrash;                           //현재 충돌 중인가?
 
-    private int num;
+    private int dropNum;                            //떨어지는 물품 개수
 
- //   public int goodsCount;
 
 
     private Vector3 goodsDropPos;
@@ -22,7 +21,7 @@ public class WagonCtrl : MonoBehaviour
     void Start()
     {
         isCrash = false;
-        num = 0;
+        dropNum = 0;
 
         range[0] = 20;
         range[1] = 100;
@@ -34,61 +33,51 @@ public class WagonCtrl : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     void OnCollisionEnter(Collision coll)
     {
-        
-        Debug.Log(coll.relativeVelocity.sqrMagnitude);
 
-        Debug.Log("coll 벡터 : " + coll.relativeVelocity);
-
-
-        //num = 1;
-
+        //충돌 벡터의 힘에 따라 수송 물품이 떨어지는 개수 결정
         if (coll.relativeVelocity.sqrMagnitude > range[0] && coll.relativeVelocity.sqrMagnitude <= range[1])
         {
-            num = 1;
+            dropNum = 1;
         }
         else if (coll.relativeVelocity.sqrMagnitude > range[1] && coll.relativeVelocity.sqrMagnitude <= range[2])
         {
-            num = 3;
+            dropNum = 3;
         }
         else if (coll.relativeVelocity.sqrMagnitude > range[2] && coll.relativeVelocity.sqrMagnitude <= range[3])
         {
-            num = 5;
+            dropNum = 5;
         }
         else if (coll.relativeVelocity.sqrMagnitude > range[3] && coll.relativeVelocity.sqrMagnitude <= range[4])
         {
-            num = 8;
+            dropNum = 8;
         }
         else if (coll.relativeVelocity.sqrMagnitude > range[4])
         {
-            num = 10;
+            dropNum = 10;
         }
 
 		Item item =  GameMng.Inst.getItem( 0 );
 		
-		if ( item != null && (num > item.count|| item.count == 0))
+		if ( item != null && (dropNum > item.count|| item.count == 0))
         {
-            num = item.count;
+            dropNum = item.count;
         }
-        if ( item != null && isCrash == false&&coll.gameObject.tag==("BUILDING")&&num!=0)
+
+
+        //수송 물품 생성
+        if ( item != null && isCrash == false&&coll.gameObject.tag==("BUILDING")&& dropNum != 0)
         {
             goodsDropPos = transform.position;
-            goodsDropPos.y += 0.8f;// 0.8f;
-            Debug.Log(num+"마리가 떨어집니다.");
-            goods = new GameObject[num];
-            for (int i = 0; i < num; i++)
-            {
-			
-                goods[i] = (GameObject)Instantiate(good, goodsDropPos, Quaternion.identity);
+            goodsDropPos.y += 0.8f;
+            Debug.Log(dropNum + "마리가 떨어집니다.");
+            goods = new GameObject[dropNum];
+            for (int i = 0; i < dropNum; i++)
+            {	
+                goods[i] = (GameObject)Instantiate(preObj, goodsDropPos, Quaternion.identity);
 				goods[i].SendMessage("fall", coll.relativeVelocity);
-				//goods[i].SendMessage( "fall", Vector3.Reflect( coll.relativeVelocity, coll.contacts[0].normal ) );
 				item.count -= 1;
             }
 
